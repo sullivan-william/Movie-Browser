66692c8612224a03316d52df794e4f917691461e
William Sullivan <sullivanwp95@gmail.com>
2022-04-18 16:58:09 -0400

    building components with stub content

---
+++ src/components/PopularMovies.js
@@ -0,0 +1,12 @@
+function PopularMovies() {
+
+    // function here to retrieve list of trending movies from server via api
+
+    return (
+        <div>
+            <h3>List of trending movies here</h3>
+        </div>
+    )
+}
+
+export default PopularMovies
---
+++ src/components/Reviews.js
@@ -0,0 +1,12 @@
+function Reviews() {
+
+    // function to return list of reviews from server
+
+    return (
+        <div>
+            <h3>Reviews for movie go here</h3>
+        </div>
+    )
+}
+
+export default Reviews
