5ff4ee6df34d3a8ee7c427e9f3b965b425e80423
Rodrigo Reyes <rodrigoreyes96@gmail.com>
2022-04-23 09:58:07 -0400

    add favorites

--- src/App.js
+++ src/App.js
@@ -1,95 +1,25 @@
 import React, { useState, useEffect } from 'react'
-// import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages';
-import About from './pages/about';
-import Favorites from './pages/favorites';
-import Releases from './pages/releases';
-import Popular from './pages/popular';
+// import Navbar from './components/Navbar';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import Home from './pages';
+// import About from './pages/about';
+// import Favorites from './pages/favorites';
+// import Releases from './pages/releases';
+// import Popular from './pages/popular';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
+import AddFavorites from './components/AddFavorites';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
-  //   {
-  //     "Title": "Star Wars",
-  //     "Year": "1977",
-  //     "imdbID": "tt0076759",
-  //     "Type": "movie",
-  //     "Poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-  //   },
-  //   {
-  //     "Title": "Star Wars: Episode V - The Empire Strikes Back",
-  //     "Year": "1980",
-  //     "imdbID": "tt0080684",
-  //     "Type": "movie",
-  //     "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-  //   },
-  //   {
-  //     "Title": "Star Wars: Episode VI - Return of the Jedi",
-  //     "Year": "1983",
-  //     "imdbID": "tt0086190",
-  //     "Type": "movie",
-  //     "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-  //   },
-  //   {
-  //     "Title": "Star Wars: Episode VII - The Force Awakens",
-  //     "Year": "2015",
-  //     "imdbID": "tt2488496",
-  //     "Type": "movie",
-  //     "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
-  //   },
-  //   {
-  //     "Title": "Star Wars: Episode I - The Phantom Menace",
-  //     "Year": "1999",
-  //     "imdbID": "tt0120915",
-  //     "Type": "movie",
-  //     "Poster": "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-  //   },
-  //   {
-  //     "Title": "Star Wars: Episode III - Revenge of the Sith",
-  //     "Year": "2005",
-  //     "imdbID": "tt0121766",
-  //     "Type": "movie",
-  //     "Poster": "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"
-  //   },
-  //   {
-  //     "Title": "Star Wars: Episode II - Attack of the Clones",
-  //     "Year": "2002",
-  //     "imdbID": "tt0121765",
-  //     "Type": "movie",
-  //     "Poster": "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg"
-  //   },
-  //   {
-  //     "Title": "Star Wars: Episode VIII - The Last Jedi",
-  //     "Year": "2017",
-  //     "imdbID": "tt2527336",
-  //     "Type": "movie",
-  //     "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg"
-  //   },
-  //   {
-  //     "Title": "Rogue One: A Star Wars Story",
-  //     "Year": "2016",
-  //     "imdbID": "tt3748528",
-  //     "Type": "movie",
-  //     "Poster": "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg"
-  //   },
-  //   {
-  //     "Title": "Star Wars: Episode IX - The Rise of Skywalker",
-  //     "Year": "2019",
-  //     "imdbID": "tt2527338",
-  //     "Type": "movie",
-  //     "Poster": "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SX300.jpg"
-  //   },
-  // ]);
+  const [favorites, setFavorites] = useState([]);
 
-  const getMovieRequest = async () => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&`;
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=b3573849`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -100,11 +30,15 @@ const App = () => {
 
   };
 
+
   useEffect(() => {
 		getMovieRequest(searchValue);
 	}, [searchValue]);
 
-
+  const addFavoriteMovie = (movie) => {
+		const newFavoriteList = [...favorites, movie];
+		setFavorites(newFavoriteList);
+	};
 
   return (
     // <Router>
@@ -122,9 +56,23 @@ const App = () => {
         <MovieListHeading heading='What The Film!'/>
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
-      <div className='row'>
-        <MovieList movies={movies} />
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieList
+					movies={movies}
+					handleFavoritesClick={addFavoriteMovie}
+					favoriteComponent={AddFavorites}
+				/>
+			</div>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='My Favorites'/>
       </div>
+      <div className='row'>
+				<MovieList
+					movies={favorites}
+					handleFavoritesClick={addFavoriteMovie}
+					favoriteComponent={AddFavorites}
+				/>
+			</div>
     </div>
   );
 };
