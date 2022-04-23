cbcee8dfcead9d6713c0cfef37ead4be070d0b3a
William Sullivan <sullivanwp95@gmail.com>
2022-04-23 11:30:26 -0400

    building out components with links

--- src/components/PopularMovies.js
+++ src/components/PopularMovies.js
@@ -1,10 +1,38 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
 function PopularMovies() {
 
+    const [movieData, setMovieData] = useState([])
+
     // function here to retrieve list of trending movies from server via api
+    useEffect(() => {
+        const API_URL = 'http://localhost:4000/movie/popular'
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setMovieData(resData.results)
+        }
+        fetchData()
+    }, [])
+
+    const popularMovies = movieData.map((movie, i) => {
+        return (
+            <div key={i}>
+                <p>
+                    <Link to={`/movie/${movie.id}`}>
+                        {movie.original_title}
+                    </Link>
+                </p>
+                <img src={movie.backdrop_path} alt={movie.original_title}></img>
+            </div>
+        )
+    })
 
     return (
         <div>
             <h3>List of trending movies here</h3>
+            {movieData.length > 0 ? popularMovies : <h2>Loading...</h2>} 
         </div>
     )
 }
